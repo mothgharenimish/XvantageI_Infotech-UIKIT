e466c66acb650f9ddf5489912056a9b303313be1
mothgharenimish <mothgharenimish0@gmail.com>
2025-05-21 16:11:38 +0530

    Initial Commit

---
+++ XvantageI_Infotech-UIKIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XvantageI_Infotech-UIKIT
+//
+//  Created by Nimish Mothghare on 21/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ XvantageI_Infotech-UIKITTests/XvantageI_Infotech_UIKITTests.swift
@@ -0,0 +1,17 @@
+//
+//  XvantageI_Infotech_UIKITTests.swift
+//  XvantageI_Infotech-UIKITTests
+//
+//  Created by Nimish Mothghare on 21/05/25.
+//
+
+import Testing
+@testable import XvantageI_Infotech_UIKIT
+
+struct XvantageI_Infotech_UIKITTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
