f0024b6db7ff2aeb2ae87eb9dde81390701758f3
mothgharenimish <mothgharenimish0@gmail.com>
2025-05-21 19:59:03 +0530

    Xcode updated changes

---
+++ XvantageI_Infotech-UIKIT/Controller/Users/UserListVC.swift
@@ -0,0 +1,29 @@
+//
+//  UserListVC.swift
+//  XvantageI_Infotech-UIKIT
+//
+//  Created by Nimish Mothghare on 21/05/25.
+//
+
+import UIKit
+
+class UserListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ XvantageI_Infotech-UIKIT/Controller/Users/UserXib/UserTableVC.swift
@@ -0,0 +1,23 @@
+//
+//  UserTableVC.swift
+//  XvantageI_Infotech-UIKIT
+//
+//  Created by Nimish Mothghare on 21/05/25.
+//
+
+import UIKit
+
+class UserTableVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
