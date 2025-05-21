c50044a69b9207bd612b481b7e74baf39c70e794
mothgharenimish <mothgharenimish0@gmail.com>
2025-05-21 19:59:29 +0530

    Xcoded Updated Changes

--- XvantageI_Infotech-UIKIT/Controller/Users/UserListVC.swift
+++ XvantageI_Infotech-UIKIT/Controller/Users/UserListVC.swift
@@ -8,22 +8,24 @@
 import UIKit
 
 class UserListVC: UIViewController {
-
+    
+    //MARK: -IBOutlet
+    @IBOutlet weak var backBtn: UIButton!
+    @IBOutlet weak var userstblView: UITableView!
+    
+    //MARK: -View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    //MARK: -Back IBAction 
+    @IBAction func backAction(_ sender: UIButton) {
+        
+        
     }
-    */
+    
 
 }
